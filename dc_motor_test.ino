1ce1b5b895efd89e8489a1fd38d38608d627e48c
Youngmok Yun <mok@harmonicbionics.com>
2021-05-03 02:18:15 -0500

    arduino can be controlled by the computer but response is very slow

--- dc_motor_test.ino
+++ dc_motor_test.ino
@@ -2,6 +2,8 @@ int m1_c1 = 3;
 int m1_c2 = 5;
 int m2_c1 = 6;
 int m2_c2 = 9;
+
+int incomingByte =5;
   
 void setup() {
   // pin mode setup
@@ -57,17 +59,55 @@ void WheelVel(int right_v, int left_v)
 void loop() {
   int i = 0;
   float d = 1.0;
+  int left_v = 0;
+  int right_v = 0;
+  int cmd = incomingByte - 48;
   
   for(i=0;i<6000;i++){
     {
-      int left_v = (int)(255.0* sin(2.0*3.14*i/3000.0));
-      int right_v = (int)(255.0* sin(2.0*3.14*i/3000.0));
-      WheelVel(0, 0);
-      Serial.print(left_v);  // prints hello with ending line break 
-      Serial.print(" ");
-      Serial.print(right_v);  // prints hello with ending line break 
-      Serial.print("\n");
+//      int left_v = (int)(255.0* sin(2.0*3.14*i/3000.0));
+//      int right_v = (int)(255.0* sin(2.0*3.14*i/3000.0));
+  switch (cmd) {
+    case 7:    // your hand is on the sensor
+      left_v = 128;
+      right_v = 255;
+      break;
+    case 8:    // your hand is on the sensor
+      left_v = 255;
+      right_v = 255;
+      break;
+    case 9:    // your hand is on the sensor
+      left_v = 255;
+      right_v = 128;
+      break;
+    case 4:    // your hand is on the sensor
+      left_v = 0;
+      right_v = 255;
+      break;
+    case 5:    // your hand is on the sensor
+      left_v = 0;
+      right_v = 0;
+      break;
+    case 6:    // your hand is on the sensor
+      left_v = 255;
+      right_v = 0;
+      break;
+
+  }
+      WheelVel(left_v, right_v);
+//      Serial.print(left_v);  // prints hello with ending line break 
+//      Serial.print(" ");
+//      Serial.print(right_v);  // prints hello with ending line break 
+//      Serial.print("\n");
       delay(d);
+      if (Serial.available() > 0) {
+        // read the incoming byte:
+        incomingByte = Serial.read();
+    
+        // say what you got:
+        Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+      }
     }
   }
 
