40961ee0746befa3a15c62c1c4be80e5a8410967
Youngmok Yun <mok@harmonicbionics.com>
2021-05-23 16:18:48 -0500

    updated for new hardware configuration

--- dc_motor_test.ino
+++ dc_motor_test.ino
@@ -1,22 +1,24 @@
-int m1_c1 = 3;
-int m1_c2 = 5;
-int m2_c1 = 6;
-int m2_c2 = 9;
+int m1_d = 6;
+int m1_w = 9;
+int m2_d = 3;
+int m2_w = 5;
 
 int incomingByte =5;
+int left_v = 0;
+int right_v = 0;
   
 void setup() {
   // pin mode setup
-  pinMode(m1_c1, OUTPUT);
-  pinMode(m1_c2, OUTPUT);
-  pinMode(m2_c1, OUTPUT);
-  pinMode(m2_c2, OUTPUT);
+  pinMode(m1_d, OUTPUT);
+  pinMode(m1_w, OUTPUT);
+  pinMode(m2_d, OUTPUT);
+  pinMode(m2_w, OUTPUT);
 
   // init of the pin outputs
-  digitalWrite(m1_c1, LOW);
-  digitalWrite(m1_c2, LOW);
-  digitalWrite(m2_c1, LOW);
-  digitalWrite(m2_c2, LOW);  
+  digitalWrite(m1_d, LOW);
+  digitalWrite(m1_w, LOW);
+  digitalWrite(m2_d, LOW);
+  digitalWrite(m2_w, LOW);  
 
   Serial.begin(57600);           // set up Serial library at 9600 bps
   Serial.println("Hello Arduino Code is starting");  // prints hello with ending line break 
@@ -26,13 +28,13 @@ void RightWheelVel(int v)  // v needs to be between -255 to 255
 {
   if (v>0)
   {
-    digitalWrite(m1_c1, LOW); 
-    analogWrite(m1_c2, v);
+    digitalWrite(m2_d, LOW); 
+    analogWrite(m2_w, v);
   }
   else
   {
-    analogWrite(m1_c1, -v);  
-    digitalWrite(m1_c2, LOW); 
+    digitalWrite(m2_d, HIGH); 
+    analogWrite(m2_w, -v);
   }
 }
 
@@ -40,14 +42,14 @@ void LeftWheelVel(int v)  // v needs to be between -255 to 255
 {
   if (v>0)
   {
-    analogWrite(m2_c1, v);
-    digitalWrite(m2_c2, LOW); 
+    digitalWrite(m1_d, HIGH); 
+    analogWrite(m1_w, v);
   }
   else
   {
-    digitalWrite(m2_c1, LOW); 
-    analogWrite(m2_c2, -v);  
-  }   
+    digitalWrite(m1_d, LOW); 
+    analogWrite(m1_w, -v);
+  }
 }
 
 void WheelVel(int right_v, int left_v)
@@ -58,50 +60,58 @@ void WheelVel(int right_v, int left_v)
 
 void loop() {
   int i = 0;
-  float d = 1.0;
-  int left_v = 0;
-  int right_v = 0;
+  int d = 4;
+
   int cmd = incomingByte - 48;
   
   switch (cmd) 
   {
     case 7:    // your hand is on the sensor
-      left_v = 50;
-      right_v = 255;
+      right_v++;
+      left_v = right_v * 0.3;
       break;
     case 8:    // your hand is on the sensor
-      left_v = 255;
-      right_v = 255;
+      left_v++;
+      right_v++;
       break;
     case 9:    // your hand is on the sensor
-      left_v = 255;
-      right_v = 50;
+      left_v++;
+      right_v = left_v*0.3;
       break;
     case 4:    // your hand is on the sensor
-      left_v = -255;
-      right_v = 255;
+      left_v--;
+      right_v++;
       break;
     case 5:    // your hand is on the sensor
-      left_v = 0;
-      right_v = 0;
+      if(left_v>0) left_v--;
+      else if (left_v<0) left_v++;
+      else left_v = 0;
+      if(right_v>0) right_v--;
+      else if (right_v<0) right_v++;
+      else right_v = 0;
       break;
     case 6:    // your hand is on the sensor
-      left_v = 255;
-      right_v = -255;
+      left_v++;
+      right_v--;
       break;
     case 1:    // your hand is on the sensor
-      left_v = -50;
-      right_v = -255;
+      right_v--;
+      left_v = right_v*0.3;
       break;
     case 2:    // your hand is on the sensor
-      left_v = -255;
-      right_v = -255;
+      left_v--;
+      right_v--;
       break;
     case 3:    // your hand is on the sensor
-      left_v = -255;
-      right_v = -50;
+      left_v--;
+      right_v=left_v*0.3;
       break;
   }
+  if(left_v >254) left_v = 254;
+  if(right_v > 254) right_v = 254;
+
+  if(left_v < -254) left_v = -254;
+  if(right_v < -254) right_v = -254;
 
   WheelVel(left_v, right_v);
   if (Serial.available() > 0) 
@@ -113,4 +123,6 @@ void loop() {
     Serial.print("I received: ");
     Serial.println(incomingByte, DEC);
   }
+
+  delay(d);
 }
